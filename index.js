9dd7e475be4369be4d112dd213f076a795241988
ViccyAce <adeviccy@gmail.com>
2024-08-13 04:21:57 -0200

    first deploy

---
+++ index.js
@@ -0,0 +1,12 @@
+let string1 = " 30 days of JavaScript ";
+console.log(string1);
+
+let newString2 = string1.replaceAll(
+  " 30 days of JavaScript ",
+  "30daysofJavaScript"
+);
+
+console.log(newString2);
+
+let newString3 = newString2.split();
+newString3.
